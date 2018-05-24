511c88f044454e72e04e0b09c309e62784995c84
medlinj <medlinj@rose-hulman.edu>
2018-05-24 13:13:45 -0400

    Done

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Final exam, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and JD Medlin.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,7 +19,7 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem1a()
+    run_test_problem1a()
     # run_test_problem1b()
 
 
@@ -94,6 +94,8 @@ def problem1a(numbers):
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
+    total = numbers[0] + numbers[len(numbers)//2] + numbers[len(numbers)-1]
+    return total
 
 def run_test_problem1b():
     """ Tests the  problem1b   function. """
